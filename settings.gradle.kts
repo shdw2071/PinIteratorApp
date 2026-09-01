c7395b38a0869d1298fc26faa89f679e342bfedd
shdw2071 <pulsar2001@protonmail.com>
2026-09-01 17:41:24 +0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+```kotlin
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "PinIteratorApp"
+include(":app")
+```
